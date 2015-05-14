b07760d7b3a4db5750de9e5f2f2b49bc1e4711e1
miguelbrb <miguelbrb@users.noreply.github.com>
2015-05-14 21:15:24 +0200

    Update usuario.h
    Añadido variable para llamar a los siguientes usuarios mediante memoria dinamica.
    Nuevos comentarios.
    Definido punterio para recorrer lista de usuarios.
    Funcion que vuelca los usuarios del fichero Usuarios.txt.

--- EstructuraDatos/usuario.h
+++ EstructuraDatos/usuario.h
@@ -1,12 +1,19 @@
-#ifndef USUARIO_H
-#define USUARIO_H
+#ifndef _USUARIO_H_
+#define _USUARIO_H_
 
+//Estructuras para almacenar los datos de los Usuarios
 typedef struct {
-    int identificador;
+    int id;
     char nombre_plantilla[30];
-    char perfil_usuario[20];
-    char usuario[5];
-    char contraseña[8];
+    char perfil_user[20];
+    char user[5];
+    char pass[8];
+    struct T_usuario *siguiente;
 }T_usuario;
 
-#endif
+typedef T_usuario *ListaUser;
+
+//Declaraciones de funciones
+void Leer_user(ListaUser*);
+
+#endif //_USUARIO_H_
