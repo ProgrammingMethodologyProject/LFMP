59cff6cf2e1f5db7eeb764d17e0e9513d912123c
Luis <lflla@users.noreply.github.com>
2015-05-14 17:50:53 +0200

    Create plantilla.h

---
+++ EstructuraDatos/plantilla.h
@@ -0,0 +1,12 @@
+#ifndef PLANTILLA_H
+#define PLANTILLA_H
+
+typedef struct {
+    int identificador_propietario;
+    int identificador;
+    char nombre_equipo[30];
+    int presupuesto;
+    int puntuacion;
+}T_plantilla;
+
+#endif
