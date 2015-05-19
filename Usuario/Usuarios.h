d447d9cd47d42ac65949034968353e8b8a603438
miguelbrb <miguelbrb@users.noreply.github.com>
2015-05-19 20:29:23 +0200

    Update Usuarios.h

--- Usuario/Usuarios.h
+++ Usuario/Usuarios.h
@@ -5,19 +5,20 @@
 //Estructuras para almacenar los datos de los Usuarios
 typedef struct {
     int id;
-    char nombre[20];
-    char perfil[13];
-    char user[5];
-    char pass[8];
+    char *nombre;
+    char *perfil;
+    char user[6];
+    char pass[9];
     struct T_usuario *siguiente;
 }T_usuario;
 
-typedef T_usuario *pusuarios;
+typedef T_usuario *pUsuarios;
 typedef T_usuario *Lista;
 
 //Declaraciones de funciones
 void Leer_user(Lista*);
-void Nuevo_user(Lista*,int,char,char,char,char);
+int ListaVacia(Lista);
+void Nuevo_user(Lista*,int,char*,char*,char*,char*);
 void Alta_user(Lista*);
 void Login_user(Lista*);
 int Comprobar_user(Lista*,char*,char*);
