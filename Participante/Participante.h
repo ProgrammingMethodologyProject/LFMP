56d6f7de198d9dd66415aae841aa87fa2d0649fe
indc9 <indc94@gmail.com>
2015-05-19 18:31:30 +0200

    Update Participante.h

--- Participante/Participante.h
+++ Participante/Participante.h
@@ -1,1 +1,14 @@
+#ifndef PLANTILLA_H
+#define PLANTILLA_H
 
+typedef struct {
+    int id;
+    int ident[3];
+    char nombre_equipo[30];
+    int valor_config; //presupuesto
+    int puntuacion = 0;
+}T_plantilla;
+
+T_plantilla *plantilla
+
+#endif
