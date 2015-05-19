0966298eb1df6a28ecaa46e43d54fb5dd1c0e3db
indc9 <indc94@gmail.com>
2015-05-19 19:31:22 +0200

    Update plantilla.h

--- EstructuraDatos/plantilla.h
+++ EstructuraDatos/plantilla.h
@@ -2,11 +2,13 @@
 #define PLANTILLA_H
 
 typedef struct {
-    int identificador_propietario;
-    int ident;
+    int id;
+    int ident[3];
     char nombre_equipo[30];
-    int presupuesto;
-    int puntuacion;
+    int valor_config; //presupuesto
+    int puntuacion = 0;
 }T_plantilla;
+
+T_plantilla *plantilla;
 
 #endif
