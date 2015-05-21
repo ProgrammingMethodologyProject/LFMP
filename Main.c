4ef95b7332a8b6383053a0b08b4a7b3e11a88fc7
miguelbrb <miguelbrb@users.noreply.github.com>
2015-05-21 16:54:03 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -11,10 +11,48 @@ int main(){
         switch(menu(0)){ // Menu(0) nos devuelve la opcion de navegacion por el men principal
             case 1: Alta_user(&l_usuarios); // Registro de nuevo usuario
                     system("cls"); break;
-            case 2: Login_user(&l_usuarios); // Acceso con usuario existente
-                    system("cls"); break;
+            case 2: op = Login_user(&l_usuarios); // Acceso con usuario existente
+                    S=0; break;
             case 3: //Salir
                     S=0; break;
+        }
+    }
+    S=1;
+    while(S){
+        if(op == 1){
+            switch(menu(op)){
+                case 1: // Equipos
+                        system("pause"); system("cls"); break;
+                case 2: // Usuarios
+                        system("pause"); system("cls"); break;
+                case 3: // Configuracion
+                        system("pause"); system("cls"); break;
+                case 4: S=0; system("cls"); break;
+            }
+        }
+        if(op == 2){
+            switch(menu(op)){
+                case 1: //
+                        system("pause"); system("cls"); break;
+                case 2: //
+                        system("pause"); system("cls"); break;
+                case 3: S=0; system("cls"); break;
+            }
+        }
+        if(op == 3){
+            switch(menu(op)){
+                case 1: //
+                        system("pause"); system("cls"); break;
+                case 2: // system("pause");
+                        system("cls"); break;
+                case 3: // system("pause");
+                        system("cls"); break;
+                case 4: // system("pause");
+                        system("cls"); break;
+                case 5: // system("pause");
+                        system("cls"); break;
+                case 6: S=0; system("cls"); break;
+            }
         }
     }
 return 0;
