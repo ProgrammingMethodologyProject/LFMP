522c011e4cb71e09815f2e4ed0c8959ddf31d7c7
miguelbrb <miguelbrb@users.noreply.github.com>
2015-05-21 12:00:22 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -4,18 +4,17 @@
 #include "Usuarios.h"
 
 int main(){
-    T_usuario *head = NULL;
-    Leer_user(head); //Funcion que recorre el fichero Usuarios.txt
+    Lista l_usuarios=NULL; // Indice para el listado de usuarios
+    Leer_user(&l_usuarios); //Funcion que recorre el fichero Usuarios.txt
     int S=1;
     while(S){
         switch(menu(0)){ // Menu(0) nos devuelve la opcion de navegacion por el men principal
-            case 1: // Menu(1) de registro
+            case 1: Alta_user(&l_usuarios); // Registro de nuevo usuario
                     system("cls"); break;
-            case 2: // Menu(2) de acceso
+            case 2: Login_user(&l_usuarios); // Acceso con usuario existente
                     system("cls"); break;
             case 3: //Salir
-                    S=0;
-                    system("cls"); break;
+                    S=0; break;
         }
     }
 return 0;
