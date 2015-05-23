c08298789d3108c0efd70653c9f59458c7257738
miguelbrb <miguelbrb@users.noreply.github.com>
2015-05-23 16:14:02 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -10,7 +10,7 @@ int main(){
     while(S){
         switch(menu(0)){ // Menu(0) nos devuelve la opcion de navegacion por el men principal
             case 1: Alta_user(&l_usuarios); // Registro de nuevo usuario
-                    system("cls"); break;
+                    getch(); system("cls"); break;
             case 2: op = Login_user(&l_usuarios); // Acceso con usuario existente
                     S=0; break;
             case 3: //Salir
@@ -19,40 +19,56 @@ int main(){
     }
     S=1;
     while(S){
-        if(op == 1){
-            switch(menu(op)){
-                case 1: // Equipos
-                        system("pause"); system("cls"); break;
-                case 2: // Usuarios
-                        system("pause"); system("cls"); break;
-                case 3: // Configuracion
-                        system("pause"); system("cls"); break;
-                case 4: S=0; system("cls"); break;
+        if(op == 0){ S=0; } // Salir
+        if(op == 1){ //Menu administrador
+            op = menu(op); system("cls");
+            while(op == 1){ // Equipos
+                switch(menu(4)){
+                    case 1: // Listar Equipos
+                            getch(); system("cls"); op = 1; break;
+                    case 2: // Modificar equipos
+                            getch(); system("cls"); op = 1; break;
+                    case 3: // Agregar equipos
+                            getch(); system("cls"); op = 1; break;
+                    case 4: // Eliminar equipos
+                            getch(); system("cls"); op = 1; break;
+                    case 5: S=0; system("cls"); op = 0; break;
+                }
             }
+            while(op == 2){ // Usuarios
+                switch(menu(5)){
+                    case 1: Listar_user(l_usuarios); // Listar usuarios
+                            getch(); system("cls"); op = 2; break;
+                    case 2: Localizar_user(&l_usuarios);// Modificar usuarios
+                            getch(); system("cls"); op = 2; break;
+                    case 3: Alta_user(&l_usuarios);// Agregar usuarios
+                            getch(); system("cls"); op = 2; break;
+                    case 4: Eliminar_user(&l_usuarios);// Eliminar usuarios
+                            getch(); system("cls"); op = 2; break;
+                    case 5: system("cls"); op = 0; break;
+                }
+            }
+            while(op == 3){ //Configuracion
+                switch(menu(6)){
+                    case 1: // Numero maximo de equipos
+                            getch(); system("cls"); op = 3; break;
+                    case 2: // Presupuesto por defecto
+                            getch(); system("cls"); op = 3; break;
+                    case 3: // Numero maximo de jugadores por equipo
+                            getch(); system("cls"); op = 3; break;
+                    case 4: system("cls"); op = 0; break;
+                }
+            }
+            if(op == 4){ S=0; }
+            op = 1;
         }
-        if(op == 2){
-            switch(menu(op)){
-                case 1: //
-                        system("pause"); system("cls"); break;
-                case 2: //
-                        system("pause"); system("cls"); break;
-                case 3: S=0; system("cls"); break;
-            }
+        if(op == 2){ // Menu cronista
+            op = menu(op); system("cls");
+            op = 2;
         }
-        if(op == 3){
-            switch(menu(op)){
-                case 1: //
-                        system("pause"); system("cls"); break;
-                case 2: // system("pause");
-                        system("cls"); break;
-                case 3: // system("pause");
-                        system("cls"); break;
-                case 4: // system("pause");
-                        system("cls"); break;
-                case 5: // system("pause");
-                        system("cls"); break;
-                case 6: S=0; system("cls"); break;
-            }
+        if(op == 3){ // Menu participante
+            op = menu(op); system("cls");
+            op = 3;
         }
     }
 return 0;
