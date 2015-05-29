ad10d8b78c6c88a056a439ff9fcc8149be170e55
miguelbrb <miguelbrb@users.noreply.github.com>
2015-05-29 16:17:26 +0200

    Update Main.c

--- Main.c
+++ Main.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <conio.h>
 #include "menu.h"
 #include "Usuarios.h"
 #include "Equipos.h"
+#include "Configuracion.h"
 
 int main(){
     ListaE l_equipos=NULL; // Indice para el listado de equipos
     ListaU l_usuarios=NULL; // Indice para el listado de usuarios
+    ListaC l_config=NULL; // Indice para el listado de la configuracion
     Leer_equipo(&l_equipos); //Funcion que recorre el fichero Equipos.txt
     Leer_user(&l_usuarios); //Funcion que recorre el fichero Usuarios.txt
-    int S=1;
+    Leer_config(&l_config); //Funcion que recorre el fichero Configuracion.txt
+    int S=1,op=0;
     while(S){
         switch(menu(0)){ // Menu(0) nos devuelve la opcion de navegacion por el men principal
             case 1: Alta_user(&l_usuarios); // Registro de nuevo usuario
@@ -27,7 +31,7 @@ int main(){
             op = menu(op); system("cls");
             while(op == 1){ // Equipos
                 switch(menu(4)){
-                    case 1: Listar_equipo(l_equipos);// Listar Equipos
+                    case 1: Listar_equipo(&l_equipos);// Listar Equipos
                             getch(); system("cls"); op = 1; break;
                     case 2: Localizar_equipo(&l_equipos);// Modificar equipos
                             getch(); system("cls"); op = 1; break;
@@ -40,7 +44,7 @@ int main(){
             }
             while(op == 2){ // Usuarios
                 switch(menu(5)){
-                    case 1: Listar_user(l_usuarios); // Listar usuarios
+                    case 1: Listar_user(&l_usuarios); // Listar usuarios
                             getch(); system("cls"); op = 2; break;
                     case 2: Localizar_user(&l_usuarios);// Modificar usuarios
                             getch(); system("cls"); op = 2; break;
@@ -53,13 +57,15 @@ int main(){
             }
             while(op == 3){ //Configuracion
                 switch(menu(6)){
-                    case 1: // Numero maximo de equipos
+                    case 1: Listar_config(&l_config); // Listar todos los campos de configuracion
                             getch(); system("cls"); op = 3; break;
-                    case 2: // Presupuesto por defecto
+                    case 2: Localizar_config(&l_config,1); // Numero maximo de equipos
                             getch(); system("cls"); op = 3; break;
-                    case 3: // Numero maximo de jugadores por equipo
+                    case 3: Localizar_config(&l_config,2);// Presupuesto por defecto
                             getch(); system("cls"); op = 3; break;
-                    case 4: system("cls"); op = 0; break;
+                    case 4: Localizar_config(&l_config,3);// Numero maximo de jugadores por equipo
+                            getch(); system("cls"); op = 3; break;
+                    case 5: system("cls"); op = 0; break;
                 }
             }
             if(op == 4){ S=0; }
@@ -67,12 +73,29 @@ int main(){
         }
         if(op == 2){ // Menu cronista
             op = menu(op); system("cls");
+            if(op == 1){ Listar_equipo(&l_equipos); }// Listar Equipos
+            while(op == 2){}
+            if(op == 3){ S=0; }
             op = 2;
         }
         if(op == 3){ // Menu participante
-            op = menu(op); system("cls");
-            op = 3;
+            while(S){
+                switch(menu(3)){
+                    case 1: // Crear plantilla
+                            getch(); system("cls"); op = 3; break;
+                    case 2: // Configurar disponibles
+                            getch(); system("cls"); op = 3; break;
+                    case 3: // Listar plantilla
+                            getch(); system("cls"); op = 3; break;
+                    case 4: // Eliminar plantilla
+                            getch(); system("cls"); op = 3; break;
+                    case 5: // Ranking
+                            getch(); system("cls"); op = 3; break;
+                    case 6: system("cls"); S=0; break;
+                }
+            }
         }
     }
+    Update_equipos(&l_equipos); Update_user(&l_usuarios); Update_config(&l_config); // Actualiza los ficheros
 return 0;
 }
